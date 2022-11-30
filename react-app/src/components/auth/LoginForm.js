f866414a085069f7916e65c8a41ce6475a998b4a
Eliza <elizawimberly@gmail.com>
2022-11-29 18:00:53 -0800

    css on login

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { Redirect, NavLink } from "react-router-dom";
 //local files
 import { login } from "../../store/session";
+import "./Auth.css";
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -44,65 +45,76 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={updateEmail}
-          required={true}
-          minLength={2}
-          maxLength={50}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-          required={true}
-          minLength={2}
-          maxLength={50}
-        />
-        {/* <button type="submit">Login</button> */}
+    <div className="page-wrapper-container">
+      <div className="background-image-container">
+        {/* <img className="background-image" alt='background' src={background}></img> */}
       </div>
 
-      <div className="errors-container">
-        {errors.map((error, ind) => (
-          <div className="form-errors" key={ind}>
-            {error}
+      <div className="LoginForm-and-SignUpForm-components">
+        <div className="login-signup-form" id="login-form">
+          <div>
+            <p className="login-signup-form-prompt">Log in to Melieu</p>
           </div>
-        ))}
-      </div>
 
-      <button type="submit" className="login-signup-form-button">
-        Sign in
-      </button>
+          <form onSubmit={onLogin}>
+            <div>
+              {/* <label htmlFor="email">Email</label> */}
+              <input
+                name="email"
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={updateEmail}
+                required={true}
+                minLength={2}
+                maxLength={50}
+                className="login-signup-form-input-field"
+              />
+            </div>
+            <div>
+              {/* <label htmlFor="password">Password</label> */}
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={updatePassword}
+                required={true}
+                minLength={2}
+                maxLength={50}
+                className="login-signup-form-input-field"
+              />
+              {/* <button type="submit">Login</button> */}
+            </div>
 
-      <div onClick={demoUser} className="demo-signup-form-button">
-        Continue as Demo User
-      </div>
+            <div className="errors-container">
+              {errors.map((error, ind) => (
+                <div className="form-errors" key={ind}>
+                  {error}
+                </div>
+              ))}
+            </div>
 
-      <div className="form-redirect-prompt">
-        Not a Milieu member?{" "}
-        <span>
-          <NavLink to="/sign-up" exact={true} className="form-link">
-            Sign up here.
-          </NavLink>
-        </span>
+            <button type="submit" className="login-signup-form-button">
+              Sign in
+            </button>
+
+            <div onClick={demoUser} className="demo-signup-form-button">
+              Continue as Demo User
+            </div>
+          </form>
+
+          <div className="form-redirect-prompt">
+            Not a Milieu member?{" "}
+            <span>
+              <NavLink to="/sign-up" exact={true} className="form-link">
+                Sign up here.
+              </NavLink>
+            </span>
+          </div>
+        </div>
       </div>
-    </form>
+    </div>
   );
 };
 
